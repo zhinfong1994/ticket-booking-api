4d1cdb3714bb7b9fb6b74918b28bbd6442b723b5
Zhin Fong Mow <xiao.mow1994@gmail.com>
2026-04-04 23:25:42 +0800

    Fix venue module

--- src/venue/venue.controller.ts
+++ src/venue/venue.controller.ts
@@ -1,17 +1,22 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { VenueService } from './venue.service';
+import {
+  CreateVenueDto,
+  CreateVenueResponseDto,
+} from './dtos/create-venue.dto';
+import { GetVenueResponseDto } from './dtos/get-venue.dto';
 
 @Controller('venues')
 export class VenueController {
   constructor(private readonly service: VenueService) {}
 
   @Post()
-  create(@Body() body: { name: string }) {
-    return this.service.create(body.name);
+  create(@Body() body: CreateVenueDto): Promise<CreateVenueResponseDto> {
+    return this.service.create(body);
   }
 
   @Get()
-  findAll() {
-    return this.service.findAll();
+  find(): Promise<GetVenueResponseDto[]> {
+    return this.service.find();
   }
 }
